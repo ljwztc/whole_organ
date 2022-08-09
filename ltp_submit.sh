b5597ac563daca0263fd1f6aa9676bf096920c4f
jliu288 <jliu.ee@cityu.edu.hk>
2022-08-09 21:32:31 +0800

    update label_transfer

--- ltp_submit.sh
+++ ltp_submit.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 #SBATCH -J ltp
 #SBATCH --partition=cpu1
-#SBATCH -c 8
+#SBATCH -c 32
 #SBATCH -w node22
-
 
 source /home/jliu288/.bashrc
 conda activate cuda113
 
-python label_transfer_plus.py
+python label_transfer.py
