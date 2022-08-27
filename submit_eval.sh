532e699645a92f94705a39a0514fbb5198d657d4
jliu288 <jliu.ee@cityu.edu.hk>
2022-08-27 20:31:34 +0800

    update train

--- submit_eval.sh
+++ submit_eval.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-#SBATCH -J tr192_330
+#SBATCH -J 025_gau
 #SBATCH --partition=team1
 #SBATCH --gres=gpu:1
 #SBATCH -c 3
 #SBATCH -w node31
-#SBATCH -o out/tr192_330.out
+#SBATCH -o out/025_gau.out
 
 EXP=singlecard_adv
 PORT=1105
@@ -17,4 +17,4 @@ conda activate cuda113
 #python 
 # python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT train_adv.py --snapshot-dir ./snapshots/$EXP --log-dir ./log/$EXP  --port $PORT
 # python -m torch.distributed.launch --nproc_per_node=2 --master_port=1105 train_so.py --snapshot-dir ./snapshots/try --log-dir ./log/try  --port 1105
-python eval_cond.py
+python debug_025_gaussian.py
